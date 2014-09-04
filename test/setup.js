4e1093ffd6609d0ed5bc5fa6a9ce227605431db8
Agon Bina <agon_bina@hotmail.com>
2014-09-04 05:15:06 +0200

    Implemented initial version of .create for Collection

--- test/setup.js
+++ test/setup.js
@@ -1,9 +1,14 @@
 // Export modules to global scope as necessary (only for testing)
 if (typeof process !== 'undefined' && process.title === 'node') {
     // We are in node. Require modules.
-    expect = require('chai').expect;
+    chai = require('chai');
+    expect = chai.expect;
     sinon = require('sinon');
+    sinonChai = require('sinon-chai');
 
+    chai.use(sinonChai);
+
+    Store = require('datastore');
     Model = require('store-model');
     Collection = require('..');
 
